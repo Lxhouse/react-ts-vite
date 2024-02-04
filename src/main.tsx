a14aedd2410ba5478b39c8448cd809bd039cefb0
zhemu <lingx@ibuscloud.com>
2024-02-04 16:08:06 +0800

    feat: router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const ReactDom111 = () => <div>啦啦啦啦啦这是ReactDom111</div>;
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <App /> */}
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/ReactDom111" element={<ReactDom111 />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
